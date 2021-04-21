a0c17e07bdee49938b08919b0519af688c7801b9
Mykhailo Nester <nsmisha.dev@gmail.com>
2021-04-21 09:01:27 +0300

    - disable screenshot capture;

--- app/src/main/java/dgca/verifier/app/android/MainActivity.kt
+++ app/src/main/java/dgca/verifier/app/android/MainActivity.kt
@@ -3,6 +3,7 @@ package dgca.verifier.app.android
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         setContentView(R.layout.activity_main)
 
         requestCameraPermission()
