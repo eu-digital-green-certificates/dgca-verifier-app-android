29127bdcf8b302a878e12775ebd561507d943041
Oleksandr Sarapulov <oleksandr.sarapulov@globallogic.com>
2021-05-20 15:10:47 +0300

    Implemented trigerring of public keys sync

---
+++ app/src/main/java/dgca/verifier/app/android/settings/SettingsViewModel.kt
@@ -0,0 +1,52 @@
+/*
+ *  ---license-start
+ *  eu-digital-green-certificates / dgca-verifier-app-android
+ *  ---
+ *  Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ *  ---
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ---license-end
+ *
+ *  Created by mykhailo.nester on 4/24/21 2:54 PM
+ */
+
+package dgca.verifier.app.android.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dgca.verifier.app.android.data.VerifierRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(private val verifierRepository: VerifierRepository) :
+    ViewModel() {
+
+    private val _inProgress = MutableLiveData<Boolean>()
+    val inProgress: LiveData<Boolean> = _inProgress
+
+    fun syncPublicKeys() {
+        viewModelScope.launch {
+            _inProgress.value = true
+            withContext(Dispatchers.IO) {
+                verifierRepository.fetchCertificates()
+            }
+            _inProgress.value = false
+        }
+    }
+}
