ee0f0a1f76ef002b3825d7c02568e35d55cc1e02
oleksandrsarapulovgl <82441124+oleksandrsarapulovgl@users.noreply.github.com>
2021-05-28 10:03:06 +0300

    Added open source libraries licensing (#59)

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -38,10 +38,12 @@ object Versions {
     const val androidx_room = "2.3.0"
     const val androidx_worker_ktx = "2.5.0"
     const val hilt_version = "2.33-beta"
+    const val google_licenses_version = "0.10.4"
     const val retrofit = "2.9.0"
     const val logging_interceptor = "4.0.1"
     const val gson_converter = "2.6.0"
     const val timber = "4.7.1"
+    const val google_licenses = "17.0.0"
 
     // QR
     const val zxing = "4.2.0"
