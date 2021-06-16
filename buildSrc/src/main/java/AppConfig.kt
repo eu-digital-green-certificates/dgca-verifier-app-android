6457526a04bfb5ae7e05095cd6495dc7087d767e
Oleksandr Sarapulov <oleksandr.sarapulov@globallogic.com>
2021-06-16 15:26:05 +0300

    Version update 1.0.4(4) -> 1.0.5(5)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,8 +28,8 @@ object Config {
     const val targetSdk = 29
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 4
-    const val versionName = "1.0.4"
+    const val versionCode = 5
+    const val versionName = "1.0.5"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
