a9f80f6f32025c14903cbcc2d1714f315fa9c572
oleksandrsarapulovgl <82441124+oleksandrsarapulovgl@users.noreply.github.com>
2021-06-24 21:13:08 +0300

    Bugfix/configs screen dismiss crashes fix (#111)
    * Configs and screen dismissing crashes fixes
    
    * Version update 1.1.0(8) -> 1.1.1(9)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,8 +28,8 @@ object Config {
     const val targetSdk = 29
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 8
-    const val versionName = "1.1.0"
+    const val versionCode = 9
+    const val versionName = "1.1.1"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
