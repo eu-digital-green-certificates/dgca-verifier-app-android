d50eb48f4bc9306430bbb11249f4fef03e09a096
Oleksandr Sarapulov <oleksandr.sarapulov@globallogic.com>
2021-07-08 00:42:20 +0300

    Country selection saving

---
+++ app/src/main/java/dgca/verifier/app/android/CountriesAdapter.kt
@@ -0,0 +1,77 @@
+/*
+ *  ---license-start
+ *  eu-digital-green-certificates / dgca-verifier-app-android
+ *  ---
+ *  Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ *  ---
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ---license-end
+ *
+ *  Created by osarapulov on 7/8/21 12:01 AM
+ */
+
+package dgca.verifier.app.android
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import java.util.*
+
+/*-
+ * ---license-start
+ * eu-digital-green-certificates / dgc-certlogic-android
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ *
+ * Created by osarapulov on 08.07.21 0:01
+ */
+class CountriesAdapter(
+    private val refinedCountries: List<String>,
+    private val layoutInflater: LayoutInflater
+) : BaseAdapter() {
+
+    override fun getCount(): Int = refinedCountries.size
+
+    override fun getItem(position: Int): String = refinedCountries[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View =
+        layoutInflater
+            .inflate(android.R.layout.simple_spinner_item, parent, false)
+            .apply {
+                val textView: TextView = this.findViewById(android.R.id.text1)
+                val countryIsoCode = refinedCountries[position]
+                val locale = Locale("", countryIsoCode)
+                textView.text = locale.displayCountry
+            }
+}
--- app/src/main/java/dgca/verifier/app/android/MainActivity.kt
+++ app/src/main/java/dgca/verifier/app/android/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         setContentView(R.layout.activity_main)
     }
 
