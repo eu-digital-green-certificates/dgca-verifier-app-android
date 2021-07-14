22a91b59753e7bd19f85cca643d7dcd4cf629fc7
oleksandrsarapulovgl <82441124+oleksandrsarapulovgl@users.noreply.github.com>
2021-07-14 10:43:56 +0300

    Version update (#146)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,8 +28,8 @@ object Config {
     const val targetSdk = 29
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 13
-    const val versionName = "1.1.5"
+    const val versionCode = 14
+    const val versionName = "1.1.6"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
