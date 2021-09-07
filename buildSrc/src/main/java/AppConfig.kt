c2352ebad1b2a45cad83716ebc5e433548f5a382
Oleksandr Sarapulov <oleksandr.sarapulov@globallogic.com>
2021-09-07 11:54:13 +0300

    Config update

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,8 +28,8 @@ object Config {
     const val targetSdk = 29
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 23
-    const val versionName = "1.1.15"
+    const val versionCode = 24
+    const val versionName = "1.1.16"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
