772e448fcddb5926af31d8dd990c60dd0dcce6b0
Mykhailo Nester <nsmisha.dev@gmail.com>
2022-01-04 09:18:25 +0200

    - add etag header; (#255)
    - move loading to worker;

--- revocation/src/main/java/dcc/app/revocation/mock/MockPartition.kt
+++ revocation/src/main/java/dcc/app/revocation/mock/MockPartition.kt
@@ -26,44 +26,46 @@ import com.upokecenter.cbor.CBORObject
 
 fun getPartition(): ByteArray {
     val chunkObject = CBORObject.NewMap()
-        .Add("type", "HASHLIST".toByteArray())
-        .Add("version", "1.0".toByteArray())
-        .Add("cid", "119".toByteArray())
+        .Add("type", getBytes("HASHLIST"))
+        .Add("version", getBytes("1.0"))
+        .Add("cid", getBytes("119"))
 
     val hashListCBORObject = CBORObject.NewMap()
-        .Add("kid", "33wdd=".toByteArray())
-        .Add("x", 1.toByte())
-        .Add("y", 4.toByte())
-        .Add("chunkId", "119".toByteArray())
+        .Add("kid", getBytes("33wdd="))
+        .Add("x", CBORObject.FromObject(1).AsByte())
+        .Add("y", CBORObject.FromObject(4).AsByte())
+        .Add("chunkId", getBytes(119))
         .Add(
             "hash", CBORObject.NewArray()
-                .Add("eda41aab68a44e6e71b912abc8f2fd7844a599346a9303886c5abe96f15375f4".toByteArray())
-                .Add("edd41aab68a44e6e71b912abc8f2fd7844a599346a9303886c5abe96f15375f4".toByteArray())
-                .Add("edc41aab68a44e6e71b912abc8f2fd7844a599346a9303886c5abe96f15375f4".toByteArray())
+                .Add(getBytes("eda41aab68a44e6e71b912abc8f2fd7844a599346a9303886c5abe96f15375f4"))
+                .Add(getBytes("edd41aab68a44e6e71b912abc8f2fd7844a599346a9303886c5abe96f15375f4"))
+                .Add(getBytes("edc41aab68a44e6e71b912abc8f2fd7844a599346a9303886c5abe96f15375f4"))
         )
         .Add("issued", 1640792972592)
         .Add("expired", 1640792972592)
 
     val hashListObject = CBORObject.NewMap()
-        .Add("section", "a".toByteArray())
+        .Add("section", getBytes("a"))
         .Add("hashes", hashListCBORObject)
         .Add("chunk", chunkObject)
 
     val contentObject = CBORObject.NewMap()
-        .Add("hashType", "SIGNATURE".toByteArray())
+        .Add("hashType", getBytes("SIGNATURE"))
         .Add("hashes", hashListObject)
 
     val contentMap = CBORObject.NewMap()
-        .Add("tag", "testTag".toByteArray())
+        .Add("tag", getBytes("testTag"))
         .Add("content", contentObject)
 
     val result = CBORObject.NewMap().apply {
-        Add("kid", CBORObject.FromObject("33wdd=").EncodeToBytes())
-        Add("x", 1.toByte())
-        Add("y", 4.toByte())
-        Add("version", "1.0".toByteArray())
+        Add("kid", getBytes("33wdd="))
+        Add("x", CBORObject.FromObject(1).AsByte())
+        Add("y", CBORObject.FromObject(4).AsByte())
+        Add("version", getBytes("1.0"))
         Add("meta", CBORObject.NewArray().Add(contentMap))
     }
 
     return result.EncodeToBytes()
 }
+
+fun getBytes(any: Any): ByteArray = CBORObject.FromObject(any).EncodeToBytes()
