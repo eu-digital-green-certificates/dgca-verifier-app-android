4fcb64c5e0050bf8457d6fe18490af86be80d5d9
oleksandrsarapulovgl <82441124+oleksandrsarapulovgl@users.noreply.github.com>
2022-03-30 13:23:45 +0300

    Version update 1.2.4(39) -> 1.3.0(45) (#298)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,8 +28,8 @@ object Config {
     const val targetSdk = 30
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 39
-    const val versionName = "1.2.4"
+    const val versionCode = 45
+    const val versionName = "1.3.0"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
