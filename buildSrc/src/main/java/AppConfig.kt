cfbe8ce54878bec3efc4c36bd4ce2baddb6ec4df
Mykhailo Nester <nsmisha.dev@gmail.com>
2022-04-28 09:48:59 +0300

    - version update;

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,8 +28,8 @@ object Config {
     const val targetSdk = 30
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 57
-    const val versionName = "1.4.2"
+    const val versionCode = 59
+    const val versionName = "1.3.1"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
