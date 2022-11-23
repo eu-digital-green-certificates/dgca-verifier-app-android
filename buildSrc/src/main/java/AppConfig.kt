9a516791acda7c937e2e699055ac21b3e651d118
RAW MAIN <rawmain@gmail.com>
2022-11-23 14:05:29 +0100

    Update AppConfig.kt

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -23,7 +23,7 @@
 import org.gradle.api.JavaVersion
 
 object Config {
-    const val minSdk = 26
+    const val minSdk = 23
     const val compileSdk = 31
     const val targetSdk = 30
     val javaVersion = JavaVersion.VERSION_1_8
